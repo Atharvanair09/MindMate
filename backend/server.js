92ad3830e0f2e95f1406275fab6f3f3794cfc9a3
Atharvanair09 <atharvanair09.ns@gmail.com>
2026-06-08 22:27:47 +0530

    version 4.2.0

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,7 @@ async function initNodemailer() {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
+      family: 4
     });
   } else {
     // Fallback to Ethereal Email for testing if no SMTP config is provided
