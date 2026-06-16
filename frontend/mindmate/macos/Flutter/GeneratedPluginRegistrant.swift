0dd9c884ea83413c55a7a34f9fbb261b4197a417
Atharvanair09 <atharvanair09.ns@gmail.com>
2026-06-16 19:38:48 +0530

    version 6.8.0

--- frontend/mindmate/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/mindmate/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,12 +9,10 @@ import file_selector_macos
 import flutter_secure_storage_darwin
 import flutter_tts
 import speech_to_text
-import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
-  VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
 }
